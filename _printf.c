c268f7b5a678b1dd7b11214d49a9bb3a05ab9217
umar <omarsabiu679@gmail.com>
2022-07-15 03:20:41 -0700

    Repair

--- _printf.c
+++ _printf.c
@@ -6,10 +6,9 @@
  *
  * Return: number of chars printed.
  */
-
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
+	nsigned int i = 0, len = 0, ibuf = 0;
 	va_list arguments;
 	int (*function)(va_list, char *, unsigned int);
 	char *buffer;
